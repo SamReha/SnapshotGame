383d342aa514b54f1b9a598cd4e60b2d458d0726
AndrewParvisUCSC <aparvis@ucsc.edu>
2016-01-15 13:28:10 -0800

    Camera Prefab and Basic Photo Capture
    I have implemented code that allows the user to take and store a single
    png screenshot and have most of a prefab for the camera setup. Also
    included a new standard asset ImageEffects which contains a script for
    motion blur.

---
+++ Snapshot/Assets/SnapshotCam.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnapshotCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			Application.CaptureScreenshot ("test.png");
+			print ("Oh Snapshot");
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject.FindGameObjectWithTag ("Main Camera");
+		}
+	}
+}
