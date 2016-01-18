59744b37f754c48ff8f8f204317a79faabf5208f
JamieABall <jamiegamer93@att.net>
2016-01-18 14:53:54 -0800

    Update TestSound.cs

--- Snapshot/TestSound.cs
+++ Snapshot/TestSound.cs
@@ -1,4 +1,19 @@
 using UnityEngine;
 using System.Collections
 
-public class ThrowObject: MonoBehavior
+public class ThrowObject: MonoBehavior {
+  public AudioClip clip;
+  
+  private float throw_speed = 600f; //random magic number as a placeholder
+  private AudioSource src;
+  private float volumeLow = .5f; //placeholder value
+  private float volumeHigh = 1.0f //placeholder value
+  
+  void Awake() {
+    src = GetComponent<AudioSource>();
+  }
+  
+  void Update() {
+    //to be implemented
+  }
+}
