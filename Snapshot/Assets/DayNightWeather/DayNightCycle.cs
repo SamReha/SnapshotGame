fb5e8bfeee5ce0f34d5d6045071a2f951e80839a
ryankwok <rfkwok@ucsc.edu>
2016-01-25 13:05:23 -0800

    Added Day/Night Cycle
    Known issues:
    Stars are on a sphere of radius 1000, rotating around 0,0,0. We may
    eventually need to make the sphere and stars bigger and, or make the
    heavenly bodies rotate around the player's position so that the sky
    doesn't become distorted as the player reaches the edge of the map.
    Stars are visible during the day and under the horizon.

---
+++ Snapshot/Assets/DayNightWeather/DayNightCycle.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCycle : MonoBehaviour {
+    //  Allows for control over the change in sunlight color over time
+    public Gradient nightDayColor;
+
+    //  Limits on the sunlight brightness
+    public float maxIntensity = 3f;
+    public float minIntensity = 0f;
+
+    //  Sun disappears slightly after the horizon
+    public float minPoint = -0.2f;
+
+    public float maxAmbient = 1f;
+    public float minAmbient = 0f;
+    public float minAmbientPoint = -0.2f;
+
+    public Gradient nightDayFogColor;
+    public AnimationCurve fogDensityCurve;
+    public float fogScale = 1f;
+
+    public float dayAtmosphereThickness = 0.4f;
+    public float nightAtmosphereThickness = 0.87f;
+    //  Change the speed of day/night independently
+    public float dayRotateSpeed;
+    public float nightRotateSpeed;
+    //  Adjust the skySpeed constant to speed up/slow down the time of day
+    float skySpeed = 1;
+
+    Light mainLight;
+    Skybox sky;
+    Material skyMat;
+    public ParticleSystem stars;
+    public Transform moon;
+
+	// Use this for initialization
+	void Start () {
+        mainLight = GetComponent<Light>();
+        skyMat = RenderSettings.skybox;
+	}
+	
+
+	void Update () {
+
+        //  how much time the day takes up
+        float tRange = 1 - minPoint;
+
+        //  timeOfDay returns a value between 1 and -1
+        //  where 1 is when the sun is at the highest point
+        float timeOfDay = Mathf.Clamp01((Vector3.Dot(mainLight.transform.forward, Vector3.down) - minPoint) / tRange);
+
+        //  Linearly change the sun's brightness depending on the time
+        float i = ((maxIntensity - minIntensity) * timeOfDay) * minAmbient;
+        RenderSettings.ambientIntensity = i;
+
+        //  Set the sunlight to wherever the normalized sun position falls on the gradient.
+        mainLight.color = nightDayColor.Evaluate(timeOfDay);
+
+        //  Set the ambient light to the same color as the sunlight
+        RenderSettings.ambientLight = mainLight.color;
+
+        //  Determine fog color/density depending on the time of the day
+        RenderSettings.fogColor = nightDayFogColor.Evaluate(timeOfDay);
+        RenderSettings.fogDensity = fogDensityCurve.Evaluate(timeOfDay) * fogScale;
+
+        //  Linearly change the atmosphere thickness
+        i = ((dayAtmosphereThickness - nightAtmosphereThickness) * timeOfDay) + nightAtmosphereThickness;
+        skyMat.SetFloat("_AtmosphereThickness", i);
+
+        //  Seperates the speed of night and day
+        if (timeOfDay > 0) {
+            transform.RotateAround(Vector3.zero, Vector3.right, dayRotateSpeed * Time.deltaTime * skySpeed);
+            moon.transform.RotateAround(Vector3.zero, Vector3.right, dayRotateSpeed * Time.deltaTime * skySpeed);
+        } else {
+            transform.RotateAround(Vector3.zero, Vector3.right, nightRotateSpeed * Time.deltaTime * skySpeed);
+            moon.transform.RotateAround(Vector3.zero, Vector3.right, nightRotateSpeed * Time.deltaTime * skySpeed);
+        }
+
+
+        //  For debugging purposes, map keys to control day/night cycle speed
+        if (Input.GetKeyDown(KeyCode.Q)) skySpeed *= 0.5f;
+        if (Input.GetKeyDown(KeyCode.E)) skySpeed *= 2f;
+        
+    }
+}
