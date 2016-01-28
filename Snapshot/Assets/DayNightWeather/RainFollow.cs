45f7d0245b5615c3768eb15c5f864b9a47c54783
ryankwok <rfkwok@ucsc.edu>
2016-01-28 10:43:14 -0800

    Rain Effect and Generic Animal Behavior
    I've been experimenting with a rain effect to make the weather system
    more fleshed out.  It will cost to much to simulate rain everywhere on
    the map. Instead, I'll have to create a raincloud that follows the
    player's field of vision.
    
    I also worked with Jami to create a Generic Animal behavior, complete
    with Wandering and Meandering behaviors. It still has to be tested.

---
+++ Snapshot/Assets/DayNightWeather/RainFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//  Responsible for making the rain particle emitter 
+public class RainFollow : MonoBehaviour {
+
+	public Transform playerPosition;
+	public ParticleSystem raincloud;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		raincloud.transform.position.Set (playerPosition.position.x, raincloud.transform.position.y , playerPosition.position.z);
+	}
+}
---
+++ Snapshot/Assets/GenericAgent.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenericAgent : MonoBehaviour {
+
+	Transform animalPosition;
+	NavMeshAgent agent;
+	public Vector3[] path_waypoints;// A list of waypoints that form the path of the creature
+	public float meandering_radius = 15f; //  controls the size of the area around a path waypoint that an animal meanders within
+	public float waypoint_detection_range = 5f; //  controls the size of the area around a path waypoint that an animal meanders within
+	public int meanderingExpirationTime = 100;
+	int meanderingExpirationCountdown = -1;  //  Set to negative to indicate inactive countdown
+	int currentPathIndex = 0; //  Keeps track of which wandering waypoint in the list is active
+	bool meandering = false;  //  Start the creature's wandering behavior to get to the first path
+	public Vector3 direct_waypoint;  //  This is the position that the animal always tries to follow
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		animalPosition = this.gameObject.transform;
+		//  Load in the path of the creature here
+		path_waypoints = new Vector3[3];// <--- Don't forget to update this number as the waypoint list grows
+		path_waypoints[0] =  (new Vector3(-125.5f, 0.0f, 214.4f));
+		path_waypoints[1] =  (new Vector3(-115.5f, 0.0f, 214.4f));
+		path_waypoints[2] =  (new Vector3(-125.5f, 0.0f, 150.4f));
+		//  After loading in the path of the creature, set the direct Waypoint to the first in the array
+		direct_waypoint = path_waypoints[currentPathIndex];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		meanderingExpirationCountdown--;//  Decrement the timer continuously
+		if (meandering){ //  If the animal has reached their destination waypoint,
+			float distanceToDirect = Vector3.Distance(animalPosition.position, path_waypoints[currentPathIndex]);
+			if (distanceToDirect < waypoint_detection_range){
+				//  If the animal reaches their direct waypoint while meandering, don't stop moving.
+				//  instead, give the animal another position to go to.
+				direct_waypoint = GetNearbyWaypoint(path_waypoints[currentPathIndex]);
+			}
+			if (meanderingExpirationCountdown <= 0){
+            //  If the timer has reached 0, it's time to start wandering to the next waypoint
+				currentPathIndex++; //  Tell the animal to go to the next waypoint in the path
+				if (currentPathIndex > path_waypoints.Length - 1) currentPathIndex = 0; //  Loop the animal's trail
+				meandering = false;
+			}
+		} else { //  wandering behavior. 
+			direct_waypoint = path_waypoints[currentPathIndex];
+			//  Get the distance from the path wayPoint
+			float distanceToPath = Vector3.Distance(animalPosition.position, path_waypoints[currentPathIndex]);
+			if (distanceToPath < waypoint_detection_range) {
+				//  reset the timer that will eventually stop the meandering behavior
+				meanderingExpirationCountdown = meanderingExpirationTime;
+				direct_waypoint = GetNearbyWaypoint(path_waypoints[currentPathIndex]);
+				meandering = true;
+			}
+		}
+		//  Finally, after deciding where to go tell the NavMeshAgent where to go
+		agent.SetDestination (direct_waypoint);
+	}
+
+	Vector3 GetNearbyWaypoint(Vector3 center){
+    //  returns a new Transform that is near the given position
+		return new Vector3(center.x + Random.Range(-meandering_radius, meandering_radius), 
+			                 center.y, center.z + Random.Range(-meandering_radius, meandering_radius));
+	}
+}
