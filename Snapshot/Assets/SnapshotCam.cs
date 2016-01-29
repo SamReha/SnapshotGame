264a065fc95bc55a8633e5173ae6ac13b8ac9b1c
AndrewParvisUCSC <aparvis@ucsc.edu>
2016-01-29 15:37:00 -0800

    Final Push for Sprint 1 - Andrew
    White balance, aperture, and all other objectives complete.

--- Snapshot/Assets/SnapshotCam.cs
+++ Snapshot/Assets/SnapshotCam.cs
@@ -7,7 +7,10 @@ namespace UnityStandardAssets.ImageEffects {
 	public class SnapshotCam : MonoBehaviour {
 		List<Texture2D> pics = new List<Texture2D>();
 		float[] apertureSize = {0.0f, 0.2f, 0.4f, 0.6f, 0.8f, 1.0f};
+		float[] apertureLight = {0.0f, 0.3f, 0.6f, 0.9f, 1.2f , 1.5f};
 		int apertureInt = 3;
+		Color whiteBalanceColor = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+
 		// Use this for initialization
 		void Start () {
 
@@ -28,21 +31,47 @@ namespace UnityStandardAssets.ImageEffects {
 				Destroy(rt); 
 				pics.Add (t2d);
 			}
-			if (Input.GetMouseButtonDown (0)) {
-				GameObject.FindGameObjectWithTag ("MainCamera");
-			}
 			if (Input.GetKeyDown (KeyCode.E) && apertureInt < 5) {
-				DepthOfField d = GameObject.Find ("PlayerCam").GetComponent<DepthOfField> ();
+				DepthOfField dof = GameObject.Find ("PlayerCam").GetComponent<DepthOfField> ();
+				Tonemapping tm = GameObject.Find ("PlayerCam").GetComponent<Tonemapping> ();
 				apertureInt += 1;
-				d.aperture = apertureSize [apertureInt];
-				print (d.aperture);
+				tm.exposureAdjustment = apertureLight [apertureInt];
+				dof.aperture = apertureSize [apertureInt];
+				print (dof.aperture);
 
 			}
 			if (Input.GetKeyDown (KeyCode.Q) && apertureInt > 0) {
-				DepthOfField d = GameObject.Find ("PlayerCam").GetComponent<DepthOfField> ();
+				DepthOfField dof = GameObject.Find ("PlayerCam").GetComponent<DepthOfField> ();
+				Tonemapping tm = GameObject.Find ("PlayerCam").GetComponent<Tonemapping> ();
 				apertureInt -= 1;
-				d.aperture = apertureSize [apertureInt];
-				print (d.aperture);
+				dof.aperture = apertureSize [apertureInt];
+				tm.exposureAdjustment = apertureLight [apertureInt];
+				print (dof.aperture);
+			}
+			if (Input.GetKeyDown (KeyCode.R) && whiteBalanceColor.r > 0.0f) {
+				ScreenOverlay so = GameObject.Find ("PlayerCam").GetComponent<ScreenOverlay> ();
+				Texture2D t2d = so.texture;
+				whiteBalanceColor.r = whiteBalanceColor.r - 0.01f;
+				Color[] newColor = new Color[256 * 16];
+				for (int i = 0; i < newColor.Length; i++) {
+					newColor [i] = whiteBalanceColor;
+				}
+				t2d.SetPixels(newColor);
+				t2d.Apply ();
+				print (whiteBalanceColor);
+			}
+			if (Input.GetKeyDown (KeyCode.F) && whiteBalanceColor.r < 1.0f) {
+				ScreenOverlay so = GameObject.Find ("PlayerCam").GetComponent<ScreenOverlay> ();
+				Texture2D t2d = so.texture;
+				whiteBalanceColor.r = whiteBalanceColor.r + 0.01f;
+				Color[] newColor = new Color[256 * 16];
+				for (int i = 0; i < newColor.Length; i++) {
+					newColor [i] = whiteBalanceColor;
+				}
+				t2d.SetPixels(newColor);
+				t2d.Apply ();
+				print (whiteBalanceColor);
+
 			}
 		}
 	}
