89ee26da5d3d978ccff47f775ef635fca1b7c07d
Sam Reha <samreha@dhcp-169-233-173-73.cruznet.ucsc.edu>
2016-02-12 17:58:47 -0800

    Added percent occluded field to photographable

--- Snapshot/Assets/PhotoEvaluation/Photographable.cs
+++ Snapshot/Assets/PhotoEvaluation/Photographable.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Photographable {
 	//  Parent class for any object to be analyzed in a photo
 	public float baseScore;
+	public float percentOccluded;
 	// Use this for initialization
 	public Photographable( float score ){
 		baseScore = score;
