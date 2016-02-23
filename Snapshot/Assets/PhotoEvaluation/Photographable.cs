be8dce57553c472466375ba660cb0ff342f7dffa
ryankwok <rfkwok@ucsc.edu>
2016-02-22 16:52:48 -0800

    Merged VisibilityChecker into Photographable
    Added rotation in visibility checking
    Added Aspen tree

--- Snapshot/Assets/PhotoEvaluation/Photographable.cs
+++ Snapshot/Assets/PhotoEvaluation/Photographable.cs
@@ -1,12 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Photographable : MonoBehaviour {
-	//  Parent class for any object to be analyzed in a photo
+	
+	public GameObject anObject;
+	public Collider anObjCollider;
+	private Camera cam;
+	private Plane[] planes;
+
 	public float baseScore;
 	public float percentOccluded;
+
+	public Renderer r;
+
 	// Use this for initialization
-	public Photographable( float score ){
-		baseScore = score;
+	void Start () {
+		r = GetComponent<Renderer> ();
+		r.enabled = true;
+		anObject = gameObject;
+		anObjCollider = GetComponent<Collider>();
+		cam = GameObject.Find ("PlayerCam").GetComponent<Camera> ();
 	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		planes = GeometryUtility.CalculateFrustumPlanes(cam);
+
+		if (GeometryUtility.TestPlanesAABB (planes, anObjCollider.bounds)) {
+			List<GameObject> v = GameObject.Find ("Camera Prefab").GetComponent<PhotoEval> ().visibleObjs;
+			if (!v.Contains (gameObject)) {
+				v.Add (gameObject);
+			}
+		} else {
+			List<GameObject> v = GameObject.Find ("Camera Prefab").GetComponent<PhotoEval> ().visibleObjs;
+			if (v.Contains (gameObject)) {
+				v.Remove (gameObject);
+			}
+		}
+	}
+
+
 }
