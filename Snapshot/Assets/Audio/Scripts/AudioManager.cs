ccfe012e0a25c0fd419b01429ba272d55bc6ed87
Sam Reha <samreha@dhcp-169-233-129-141.cruznet.ucsc.edu>
2016-03-03 18:56:43 -0800

    fuck

--- Snapshot/Assets/Audio/Scripts/AudioManager.cs
+++ Snapshot/Assets/Audio/Scripts/AudioManager.cs
@@ -32,11 +32,9 @@ public class AudioManager : MonoBehaviour
 	}
 
 	// Use this for initialization
-	void Start () {}
-
-	void Update () {
-		updateAnimalVolume ();
-		updateTimeOfDayTrack ();
+	void Start ()
+	{
+		
 	}
 
 	public void setDeerVolume(float value)
@@ -107,23 +105,23 @@ public class AudioManager : MonoBehaviour
 	}
 
 	/*
- 	 * Sets each animals' volume as appropriate using the following rule:
- 	 *     If the animal is further away from the player than the minThreshold, then that
- 	 *     animal is too far away to to heard and it's layer is set to 0. If the animal is
- 	 *     closer to the player than the maxThreshold, then the animal is close enough
- 	 *     that it should be heard at max volume no matter what. If the animal is in between
- 	 *     the two thresholds, then it uses the formula:
- 	 *     volume = m(x-a) + b where m = -1/(minThreshold-maxThreshold), x = distance, a = maxThreshold and b = 1.0
- 	 *     to get a value between 0.0 and 1.0 for distance between minThreshold and maxThreshold
- 	 */
+  	 * Sets each animals' volume as appropriate using the following rule:
+  	 *     If the animal is further away from the player than the minThreshold, then that
+  	 *     animal is too far away to to heard and it's layer is set to 0. If the animal is
+  	 *     closer to the player than the maxThreshold, then the animal is close enough
+  	 *     that it should be heard at max volume no matter what. If the animal is in between
+  	 *     the two thresholds, then it uses the formula:
+  	 *     volume = m(x-a) + b where m = -1/(minThreshold-maxThreshold), x = distance, a = maxThreshold and b = 1.0
+  	 *     to get a value between 0.0 and 1.0 for distance between minThreshold and maxThreshold
+  	 */
 	private void updateAnimalVolume() {
 		/*
- 		 * We can abstract this by placing all animals in a separate render layer and then
- 		 * using a Physics.OverlapSphere to get all animals within a maximum distance. Then
- 		 * we could simply get each animals' name and pass that to the audio manager with a
- 		 * volume, and the audio manager can handle mapping animal names to specific layers. 
- 		 * 		- Sam
- 		 */
+  		 * We can abstract this by placing all animals in a separate render layer and then
+  		 * using a Physics.OverlapSphere to get all animals within a maximum distance. Then
+  		 * we could simply get each animals' name and pass that to the audio manager with a
+  		 * volume, and the audio manager can handle mapping animal names to specific layers. 
+  		 * 		- Sam
+  		 */
 		float minThreshold = 60f;
 		float maxThreshold = 25f;
 		float deerDistance;
@@ -140,7 +138,7 @@ public class AudioManager : MonoBehaviour
 		}
 		if (fox == null) {
 			Debug.Log ("AudioManager: updateAnimalVolume(): Can't find fox. Perhaps the name changed?");
-		}
+			}
 		if (owl == null) {
 			Debug.Log ("AudioManager: updateAnimalVolume(): Can't find owl. Perhaps the name changed?");
 		}
@@ -170,18 +168,18 @@ public class AudioManager : MonoBehaviour
 			float volume = ((-(1/normalizedThreshold))*(foxDistance-maxThreshold)) + 1;
 			setFoxVolume (volume);
 		}
-
+			
 		/* setOwlVolume not yet implemented
- 		if (owlDistance > minThreshold) {
- 			setOwlVolume (0f);
- 		} else if (owlDistance < maxThreshold) {
- 			setOwlVolume (1f);
- 		} else {
- 			// Animal is between thresholds
- 			float volume = ((-(1/normalizedThreshold))*(owlDistance-maxThreshold)) + 1;
- 			setOwlVolume (volume);
-  		}
-  		*/
+  		if (owlDistance > minThreshold) {
+  			setOwlVolume (0f);
+  		} else if (owlDistance < maxThreshold) {
+  			setOwlVolume (1f);
+  		} else {
+  			// Animal is between thresholds
+  			float volume = ((-(1/normalizedThreshold))*(owlDistance-maxThreshold)) + 1;
+  			setOwlVolume (volume);
+   		}
+   		*/
 	}
 
 	/*
