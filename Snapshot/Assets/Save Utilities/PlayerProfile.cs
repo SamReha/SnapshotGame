0eb4515f3ca06cb4b087ea1290dbfa4f4746cb4e
Sam Reha <samreha@dhcp-169-233-129-141.cruznet.ucsc.edu>
2016-03-03 18:01:53 -0800

    Implemented simple save file module. Right now, only saves money

---
+++ Snapshot/Assets/Save Utilities/PlayerProfile.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class PlayerProfile : MonoBehaviour {
+	[Serializable]
+	public class Profile {
+		public float money;
+	}
+	public static Profile profile;
+
+	private static string saveFileName;
+
+	// Use this for initialization
+	void Start () {
+		saveFileName = "playerprofile.txt";
+		profile = new Profile ();
+	}
+
+	/*
+	 * Tries to open a save file for the current player.
+	 * In the future, this may need to consider an player
+	 * name so we can have multiple save files.
+	 */
+	public void loadProfile() {
+		if (File.Exists(saveFileName)){
+			string stringifiedProfile = File.ReadAllText (saveFileName);
+			profile = JsonUtility.FromJson<Profile>(stringifiedProfile);
+		} else {
+			Debug.Log("Save file does not exist! Creating an empty one...");
+			createProfile ();
+			saveProfile ();
+		}
+	}
+
+	/*
+	 * Tries to write to a save file for the current player.
+	 * In the future, this may need to consider a player
+	 * name so we can have multiple save files.
+	 */
+	public void saveProfile() {
+		string stringifiedProfile = JsonUtility.ToJson(profile);
+		File.WriteAllText (saveFileName, stringifiedProfile);
+	}
+
+	/*
+	 * Creates an empty profile
+	 */
+	private void createProfile() {
+		profile.money = 0;
+	}
+}
