46055aaac819d752a65be1a42430889eaed31f22
Sam Reha <samreha@dhcp-169-233-129-141.cruznet.ucsc.edu>
2016-03-03 19:03:52 -0800

    Added shell of top-level menus

---
+++ Snapshot/Assets/camera_shop/scripts/ShopUIManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopUIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	void OnGUI() {
+		//GUI.Label(Rect(0,0,100,100), "TEST");
+	}
+}
---
+++ Snapshot/Assets/main_menu/scripts/MainMenuUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	public void goToPark() {
+		Application.LoadLevel ("SSV0.0");
+	}
+
+	public void goToShop() {
+		Application.LoadLevel ("camera_shop");
+	}
+
+	public void goToBlog() {
+		Application.LoadLevel ("blog");
+	}
+}
