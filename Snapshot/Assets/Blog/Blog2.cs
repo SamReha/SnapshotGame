0aaf61a72cb12f6e8d15e991eb2ff4db7e4af4ea
Xanstus <darends@ucsc.edu>
2016-03-10 03:50:40 -0800

    Blog menu find and displays pictures taken by player.

---
+++ Snapshot/Assets/Blog/Blog2.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Blog2 : MonoBehaviour {
+
+    public GameObject newPicture;
+    [MenuItem ("AssetDatabase/Snapshot")]
+
+	// Use this for initialization
+	void Start () {
+#if UNITY_EDITOR
+        AssetDatabase.Refresh();
+#endif
+        int counter = 0;
+        DirectoryInfo dir = new DirectoryInfo("Assets");
+        FileInfo[] info = dir.GetFiles("*.png");
+        foreach (FileInfo f in info)
+        {
+            print(f);
+            string path = dir.ToString();
+            string filename = info[counter].Name;
+            print(filename);
+#if UNITY_EDITOR
+            AssetDatabase.MoveAsset(Path.Combine(path, filename), Path.Combine(Path.Combine(path, "Resources"), filename)); // move from [E....\Assets\png] name to [E....\Assets\Resources\png name]
+#endif
+            GameObject curPicture = (GameObject) Instantiate(newPicture);
+            curPicture.GetComponent<RawImage>().texture = Resources.Load(filename.Replace(".png", "")) as Texture;
+            curPicture.transform.SetParent(this.transform, false);
+            counter++;
+        }
+	}
+}
