0e8d43bca93f8de4d17d789f921948c23f4fec7a
AndrewParvisUCSC <aparvis@ucsc.edu>
2016-03-30 16:04:35 -0700

    Expansion Parsing of Rules and System Vars
    System now parses each expansion while correctly identifying whether
    each string is a word, rule or system variable. Doesn't do anything with
    it yet though.

--- Snapshot/Assets/Comments Generation/CommentGenerator.cs
+++ Snapshot/Assets/Comments Generation/CommentGenerator.cs
@@ -3,30 +3,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 public class CommentGenerator : MonoBehaviour {
 
 	string json;
 	public Comments comment = new Comments();
-	string[] delimiters = { "[[", "]]", "[", "]" };
+	char[] delimiters = { '[', ']' };
 
 
 	// Use this for initialization
 	void Start () {
 		json = System.IO.File.ReadAllText(@Application.dataPath+"/Comments Generation/Comments.json");
-		Debug.Log (json);
+		//Debug.Log (json);
 		JsonConvert.PopulateObject (json, comment);
 		foreach (KeyValuePair<string, NonTerminals> test in comment.nonTerms) {
-			Debug.Log (test.Key);
-			Debug.Log (test.Value.complete);
-			Debug.Log (test.Value.deep);
+			//Debug.Log (test.Key);
+			//Debug.Log (test.Value.complete);
+			//Debug.Log (test.Value.deep);
 			foreach (KeyValuePair<string, string[]> x in test.Value.markups) {
-				Debug.Log (x.Key);
-				Debug.Log (x.Value [0]);
+				//Debug.Log (x.Key);
+				//Debug.Log (x.Value [0]);
 			}
-			Debug.Log (test.Value.rules[0].appRate);
-			foreach (string s in test.Value.rules[0].expansion) {
-				Debug.Log (s);
+			List<Rules> r = test.Value.rules;
+			if ( r.Count > 0) {
+				//Debug.Log (test.Value.rules [0].appRate);
+				foreach (string s in test.Value.rules[0].expansion) {
+					//Debug.Log ("Expansion: " + s);
+				}
+			} else {
+				Debug.Log ("Empty Expansion");
 			}
 		}
 		foreach (NonTerminals nt in comment.nonTerms.Values) {
@@ -52,14 +58,39 @@ public class CommentGenerator : MonoBehaviour {
 	}
 
 	void parseExpansion(string word){
-		string[] words = word.Split (delimiters, StringSplitOptions.RemoveEmptyEntries);
-			if (words.Length != 1) {
-				if (words.Length != 3) {
-
-				} else {
-
-				}
-			}
+		Debug.Log ("Word: " + word);
+		IEnumerable<string> words = SplitAndKeep (word, delimiters);
+		Debug.Log ("Words: ");
+		int length = 0;
+		foreach (String w in words) {
+			Debug.Log (w);
+			length++;
+		}
+		if (length == 3) {
+			Debug.Log ("Sysvar");
+		} else if (length == 5) {
+			Debug.Log ("Rule");
+		} else {
+			Debug.Log ("Word");
+		}
 	}
+   
+   public static IEnumerable<string> SplitAndKeep( string s, char[] delims)
+    {
+        int start = 0, index;
+
+        while ((index = s.IndexOfAny(delims, start)) != -1)
+        {
+            if(index-start > 0)
+                yield return s.Substring(start, index - start);
+            yield return s.Substring(index, 1);
+            start = index + 1;
+        }
+
+        if (start < s.Length)
+        {
+            yield return s.Substring(start);
+        }
+    }
 }
 
