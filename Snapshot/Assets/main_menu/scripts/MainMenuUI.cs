aca4ee149935992f1a0bcd88243ceed9f47097a2
Samuel Reha <sreha@adgpm.soe.ucsc.edu>
2016-04-21 16:12:21 -0700

    Added badge screen to build settings and implemented some buttons

--- Snapshot/Assets/main_menu/scripts/MainMenuUI.cs
+++ Snapshot/Assets/main_menu/scripts/MainMenuUI.cs
@@ -29,4 +29,9 @@ public class MainMenuUI : MonoBehaviour {
 		mainMenuSource.Stop ();
 		SceneManager.LoadScene ("blog");
 	}
+
+    public void goToBadges() {
+        mainMenuSource.Stop();
+        SceneManager.LoadScene("badges");
+    }
 }
