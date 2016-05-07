35566a616cdc35fef8cffa330e75d70424fdbc8c
unknown <sreha@cmdrkeen.adgpm.soe.ucsc.edu>
2016-05-06 19:54:51 -0700

    CenteredBalance heuristic now returns 0 when subject is null

--- Snapshot/Assets/PhotoEvaluation/BalanceHeuristics.cs
+++ Snapshot/Assets/PhotoEvaluation/BalanceHeuristics.cs
@@ -41,7 +41,9 @@ public class BalanceHeuristics {
 	}
 
 	public static float CenteredBalance(GameObject subject, List<GameObject> visibleObjects, Camera cam) {
-		float heuristicWeight = 0.0f;
+        if (subject == null) return 0.0f;   // if by some mistake we have no subject, bail out!
+
+        float heuristicWeight = 0.0f;
         Vector3 subjectPosVector = subject.transform.position;
         List<Vector3> visibleObjectCameraSpaceCoords = new List<Vector3>();
 
@@ -49,76 +51,93 @@ public class BalanceHeuristics {
         subjectPosVector = subject.transform.TransformPoint(subject.transform.position);
         subjectPosVector = cam.WorldToViewportPoint(subjectPosVector);
 
-		if ((subjectPosVector.x <= 1.0f && subjectPosVector.x >= 0.0f) &&
-		    (subjectPosVector.y <= 1.0f && subjectPosVector.y >= 0.0f)) {
-			heuristicWeight = 5.0f;
-		} else {
-			heuristicWeight = 5.0f * 0.6f;  // hard coded for tuning purposes
-		}
+        if ((subjectPosVector.x <= 1.0f && subjectPosVector.x >= 0.0f) &&
+            (subjectPosVector.y <= 1.0f && subjectPosVector.y >= 0.0f))
+        {
+            heuristicWeight = 5.0f;
+        }
+        else
+        {
+            heuristicWeight = 5.0f * 0.6f;  // hard coded for tuning purposes
+        }
 
-		//transform all positions to viewport space
-		for (int i = 0; i < visibleObjects.Count; i++) {
+        //transform all positions to viewport space
+        for (int i = 0; i < visibleObjects.Count; i++)
+        {
             Vector3 cameraSpaceCoordinate = visibleObjects[i].transform.TransformPoint(visibleObjects[i].transform.position);
             cameraSpaceCoordinate = cam.WorldToViewportPoint(cameraSpaceCoordinate);
             visibleObjectCameraSpaceCoords.Add(cameraSpaceCoordinate);
-		}
+        }
 
         //get everything on the far left
         List<Vector3> leftmostCoords = new List<Vector3>();
-        for (int j = 0; j < visibleObjectCameraSpaceCoords.Count; j++) {
-			if (visibleObjectCameraSpaceCoords[j].x < 0.0f
+        for (int j = 0; j < visibleObjectCameraSpaceCoords.Count; j++)
+        {
+            if (visibleObjectCameraSpaceCoords[j].x < 0.0f
                 && visibleObjectCameraSpaceCoords[j].y >= 0.0f
-                && visibleObjectCameraSpaceCoords[j].y <= 1.0f) {
+                && visibleObjectCameraSpaceCoords[j].y <= 1.0f)
+            {
                 leftmostCoords.Add(visibleObjectCameraSpaceCoords[j]);
-			}
-		}
+            }
+        }
 
-		//get everything on the far right
-		List<Vector3> rightmostCoords = new List<Vector3>();
-		for (int k = 0; k < visibleObjectCameraSpaceCoords.Count; k++) {
-			if (visibleObjectCameraSpaceCoords[k].x > 1.0f
+        //get everything on the far right
+        List<Vector3> rightmostCoords = new List<Vector3>();
+        for (int k = 0; k < visibleObjectCameraSpaceCoords.Count; k++)
+        {
+            if (visibleObjectCameraSpaceCoords[k].x > 1.0f
                 && visibleObjectCameraSpaceCoords[k].y >= 0.0f
-                && visibleObjectCameraSpaceCoords[k].y <= 1.0f) {
+                && visibleObjectCameraSpaceCoords[k].y <= 1.0f)
+            {
                 rightmostCoords.Add(visibleObjectCameraSpaceCoords[k]);
-			}
-		}
+            }
+        }
 
-		//Calculate the distances between the of the leftmost and rightmost objects from the subject
-		List<float> distancesLeft = new List<float>();
-		List<float> distancesRight = new List<float>();
+        //Calculate the distances between the of the leftmost and rightmost objects from the subject
+        List<float> distancesLeft = new List<float>();
+        List<float> distancesRight = new List<float>();
 
-		for(int ii = 0; ii < leftmostCoords.Count; ii++) {
-			distancesLeft.Add(Vector3.Distance(subjectPosVector, leftmostCoords[ii]));
-		}
-			
-		for(var jj = 0; jj < rightmostCoords.Count; jj++) {
-			distancesRight.Add(Vector3.Distance(subjectPosVector, rightmostCoords[jj]));
-		}
+        for (int ii = 0; ii < leftmostCoords.Count; ii++)
+        {
+            distancesLeft.Add(Vector3.Distance(subjectPosVector, leftmostCoords[ii]));
+        }
 
-		// find the largest distances calculated on the left and the rightmost distances
-		float largestDistanceLeft = Mathf.Max(distancesLeft.ToArray());
-		float largestDistanceRight = Mathf.Max(distancesRight.ToArray());
+        for (var jj = 0; jj < rightmostCoords.Count; jj++)
+        {
+            distancesRight.Add(Vector3.Distance(subjectPosVector, rightmostCoords[jj]));
+        }
 
-		if(largestDistanceLeft == largestDistanceRight) {
-			heuristicWeight += 5.0f;
-		} else if(Mathf.Approximately(largestDistanceLeft, largestDistanceRight)) {
-			heuristicWeight += 2.0f;
-		} else {
-			heuristicWeight += 1.0f;
-		}
-		heuristicWeight *= 0.1f;
+        // find the largest distances calculated on the left and the rightmost distances
+        float largestDistanceLeft = Mathf.Max(distancesLeft.ToArray());
+        float largestDistanceRight = Mathf.Max(distancesRight.ToArray());
 
-		float actualScore = 10f * heuristicWeight;
+        if (largestDistanceLeft == largestDistanceRight)
+        {
+            heuristicWeight += 5.0f;
+        }
+        else if (Mathf.Approximately(largestDistanceLeft, largestDistanceRight))
+        {
+            heuristicWeight += 2.0f;
+        }
+        else
+        {
+            heuristicWeight += 1.0f;
+        }
+        heuristicWeight *= 0.1f;
 
-		if(actualScore == float.NaN) {
-			actualScore = 0.0f;
-			Debug.Log("CenteredBalance NaN");
-		}
+        float actualScore = 10f * heuristicWeight;
 
-        if (actualScore < 0f) {
+        if (actualScore == float.NaN)
+        {
+            actualScore = 0.0f;
+            Debug.Log("CenteredBalance NaN");
+        }
+
+        if (actualScore < 0f)
+        {
             actualScore = 0f;
         }
-        
-		return actualScore;
+
+        return actualScore;
 	}
 }
