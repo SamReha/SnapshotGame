f9da2298dde7eaa979a4524c28b402776e430c9e
ryankwok <rfkwok@adgpm.soe.ucsc.edu>
2016-05-23 14:52:27 -0700

    Implemented Lightning

---
+++ Snapshot/Assets/DayNightWeather/LightningController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightningController : MonoBehaviour {
+	
+	public WeatherControl weatherBox;
+	public Light flash;
+	private int lightningTimer = -1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int chance = Random.Range (30,1001);
+		if (chance > 995 && lightningTimer < -100) {
+			lightningTimer = 5;
+		}
+		
+		if (lightningTimer > 0) {
+			flash.enabled = true;
+		} else {
+			flash.enabled = false;
+		}
+
+		lightningTimer -= 1;
+	}
+}
