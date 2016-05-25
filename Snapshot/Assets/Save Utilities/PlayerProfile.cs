8e7ecf88e13eb870808281d64fefa9c4ef675bb7
JamieABall <jamiegamer93@att.net>
2016-05-25 15:51:34 -0700

    Update PlayerProfile.cs

--- Snapshot/Assets/Save Utilities/PlayerProfile.cs
+++ Snapshot/Assets/Save Utilities/PlayerProfile.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System;
 using System.IO;
 using System.Collections;
@@ -10,8 +10,35 @@ public class PlayerProfile : MonoBehaviour {
 
 	public static PlayerProfile profile;
 	public float money;
-	public List<string> lenses;
+    public uint memoryCardCapacity;
+	public float bagSize;
+    public List<string> lenses;
+	public List<string> filters;
 	public List<string> postedPhotos;
+	public float maxInterestTotal;
+	public float maxInterestIndividual;
+	public List<string> lensesInBag;
+	public List<string> filtersInBag;
+
+
+	//  Tutorial flags
+	public bool tutFlagMovement;
+	public bool tutFlagAim;
+	public bool tutFlagSnap;
+	public bool tutFlagLook;
+	public bool tutFlagJump;
+	public bool tutFlagRun;
+	public bool tutFlagAperture;
+	public bool tutFlagWhiteBalance;
+	public bool tutFlagChangeLens;
+	public bool tutFlagShutterSpeed;
+	public bool tutFlagViewControls;
+
+	public bool blogNamed;
+	public string blogName = "New Photographer";
+
+	public float timeElapsedInPark = 0;  //Park time added in Snapshotcam.cs
+	public float timeElapsedInMenu = 0;  //Park time added in Snapshotcam.cs
 
 	// Use this for initialization
 	void Start () {}
@@ -29,6 +56,18 @@ public class PlayerProfile : MonoBehaviour {
 		}
 	}
 
+	void Update(){
+		if (Application.loadedLevel == 1) {
+			//  Player is in the park
+			timeElapsedInPark += Time.deltaTime;
+		} else {
+			//  Player is not in the park
+			timeElapsedInMenu += Time.deltaTime;
+		}
+		//Debug.Log ("Time in park: "+ timeElapsedInPark);
+		//Debug.Log ("Time in menu: "+ timeElapsedInMenu);
+	}
+
 	/*
 	 * Tries to open a save file for the current player.
 	 * If it can't, it creates a new save file.
@@ -44,15 +83,40 @@ public class PlayerProfile : MonoBehaviour {
 
 			InternalProfile saveData = (InternalProfile)binForm.Deserialize (saveFile);
 			saveFile.Close ();
-
+			//  Load in values
 			money = saveData.money;
+            memoryCardCapacity = saveData.memoryCardCapacity;
+			bagSize = saveData.bagSize;
 			lenses = saveData.lenses;
+			filters = saveData.filters;
 			postedPhotos = saveData.postedPhotos;
+			maxInterestTotal = saveData.maxInterestTotal;
+			maxInterestIndividual = saveData.maxInterestIndividual;
+
+			tutFlagMovement = saveData.tutFlagMovement;
+			tutFlagAim = saveData.tutFlagAim;
+			tutFlagSnap = saveData.tutFlagSnap;
+			tutFlagLook = saveData.tutFlagLook;
+			tutFlagJump = saveData.tutFlagJump;
+			tutFlagRun = saveData.tutFlagRun;
+			tutFlagAperture = saveData.tutFlagAperture;
+			tutFlagWhiteBalance = saveData.tutFlagWhiteBalance;
+			tutFlagChangeLens = saveData.tutFlagChangeLens;
+			tutFlagShutterSpeed = saveData.tutFlagShutterSpeed;
+			tutFlagViewControls = saveData.tutFlagViewControls;
+
+			timeElapsedInPark = saveData.timeElapsedInPark;
+			timeElapsedInMenu = saveData.timeElapsedInMenu;
+			lensesInBag = saveData.lensesInBag;
+			filtersInBag = saveData.filtersInBag;
+			blogNamed = saveData.blogNamed;
+			blogName = saveData.blogName;
 		} else {
 			Debug.Log("Save file does not exist! Creating an empty one...");
 			createProfile ();
 		}
 	}
+
 
 	/*
 	 * Tries to write to a save file for the current player.
@@ -71,8 +135,32 @@ public class PlayerProfile : MonoBehaviour {
 
 		InternalProfile saveData = new InternalProfile ();
 		saveData.money = money;
+        saveData.memoryCardCapacity = memoryCardCapacity;
+		saveData.bagSize = bagSize;
 		saveData.lenses = lenses;
+		saveData.filters = filters;
 		saveData.postedPhotos = postedPhotos;
+		saveData.maxInterestTotal = maxInterestTotal;
+		saveData.maxInterestIndividual = maxInterestIndividual;
+
+		saveData.tutFlagMovement = tutFlagMovement;
+		saveData.tutFlagAim = tutFlagAim;
+		saveData.tutFlagSnap = tutFlagSnap;
+		saveData.tutFlagLook = tutFlagLook;
+		saveData.tutFlagJump = tutFlagJump;
+		saveData.tutFlagRun = tutFlagRun;
+		saveData.tutFlagAperture = tutFlagAperture;
+		saveData.tutFlagWhiteBalance = tutFlagWhiteBalance;
+		saveData.tutFlagChangeLens = tutFlagChangeLens;
+		saveData.tutFlagShutterSpeed = tutFlagShutterSpeed;
+		saveData.tutFlagViewControls = tutFlagViewControls;
+
+		saveData.timeElapsedInPark = timeElapsedInPark;
+		saveData.timeElapsedInMenu = timeElapsedInMenu;
+		saveData.lensesInBag = lensesInBag;
+		saveData.filtersInBag = filtersInBag;
+		saveData.blogNamed = blogNamed;
+		saveData.blogName = blogName;
 
 		binForm.Serialize (saveFile, saveData);
 		saveFile.Close ();
@@ -83,10 +171,40 @@ public class PlayerProfile : MonoBehaviour {
 	 */
 	private void createProfile() {
 		money = 0;
+        memoryCardCapacity = 8;
+		bagSize = 5;
 		lenses = new List<string> ();
+		filters = new List<string> ();
 		postedPhotos = new List<string> ();
+		maxInterestTotal = 20f;
+		maxInterestIndividual = 5f;
+		lensesInBag = new List<string> ();
+		filtersInBag = new List<string> ();
 
+		filters.Add ("clear");
 		lenses.Add ("port1");
+		filtersInBag.Add ("clear");
+		lensesInBag.Add ("port1");
+
+		//  Tutorial flags
+		tutFlagMovement = false;
+		tutFlagAim = false;
+		tutFlagSnap = false;
+		tutFlagLook = false;
+		tutFlagJump = false;
+		tutFlagRun = false;
+		tutFlagAperture = false;
+		tutFlagWhiteBalance = false;
+		tutFlagChangeLens = false;
+		tutFlagShutterSpeed = false;
+		tutFlagViewControls = false;
+
+		//blog flags
+		blogNamed = false;
+		blogName = "BlogName";
+
+		timeElapsedInPark = 0;
+		timeElapsedInMenu = 0;
 
 		save ();
 	}
@@ -99,9 +217,38 @@ public class PlayerProfile : MonoBehaviour {
  * profile.money than profile.internalprofile.money. It also allows us
  * to preform binary serialization (that way save files aren't ever user-editable)
  */
+
 [Serializable]
 class InternalProfile {
 	public float money;
+    public uint memoryCardCapacity;
+	public float bagSize;
 	public List<string> lenses;
+	public List<string> filters;
 	public List<string> postedPhotos;
+	public float maxInterestTotal;
+	public float maxInterestIndividual;
+
+	public string blogName;
+
+	//  Tutorial flags
+	public bool tutFlagMovement;
+	public bool tutFlagAim;
+	public bool tutFlagSnap;
+	public bool tutFlagLook;
+	public bool tutFlagJump;
+	public bool tutFlagRun;
+	public bool tutFlagAperture;
+	public bool tutFlagWhiteBalance;
+	public bool tutFlagChangeLens;
+	public bool tutFlagShutterSpeed;
+	public bool tutFlagViewControls;
+
+	//blog flags
+	public bool blogNamed;
+
+	public float timeElapsedInPark;
+	public float timeElapsedInMenu;
+	public List<string> lensesInBag;
+	public List<string> filtersInBag;
 }
