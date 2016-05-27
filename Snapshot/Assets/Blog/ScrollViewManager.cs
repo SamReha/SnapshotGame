0d8fdfcde8e557603b2dc4d2d6ae770cd9ddcef0
JamieABall <jamiegamer93@att.net>
2016-05-27 16:29:17 -0700

    Update ScrollViewManager.cs

--- Snapshot/Assets/Blog/ScrollViewManager.cs
+++ Snapshot/Assets/Blog/ScrollViewManager.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Collections;
@@ -10,11 +10,11 @@ using UnityEditor;
 
 public class ScrollViewManager : MonoBehaviour {
 	public GameObject newPicture;
-
 	public List<string> imagesToPost;
-
     public Toggle toggle;
     public List<GameObject> curNames = new List<GameObject>();
+
+	private string pathToUploadQueue;
 
 #if UNITY_EDITOR
     [MenuItem ("AssetDatabase/Snapshot")]
@@ -22,33 +22,9 @@ public class ScrollViewManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-#if UNITY_EDITOR
-		//  Make sure pictures are loaded into resources
-        AssetDatabase.Refresh();
-#endif
-        int counter = 0;
-		DirectoryInfo dir = new DirectoryInfo(Application.dataPath + "/Resources/");
-        //Debug.Log(dir);
-        FileInfo[] info = dir.GetFiles("*.png");
-        foreach (FileInfo f in info)
-        {
-            //Debug.Log(f);
-            string filename = f.Name;
-            //Debug.Log(filename);
+		pathToUploadQueue = Application.dataPath + "/Resources/UploadQueue/";
 
-			if (!PlayerProfile.profile.postedPhotos.Contains (filename.Replace (".png", ""))) {
-				GameObject curPicture = (GameObject)Instantiate (newPicture);
-				Texture2D pic = new Texture2D (2, 2);
-				byte[] bytes = File.ReadAllBytes (Application.dataPath + "/Resources/" + filename);
-				pic.LoadImage (bytes);
-				RawImage r = (RawImage) curPicture.GetComponent<RawImage> ();
-				r.texture = pic;
-				curPicture.GetComponent<RawImage> ().name = filename.Replace (".png", "");
-				curPicture.transform.SetParent (this.transform, false);
-				curNames.Add (curPicture);
-			}
-            counter++;
-        }
+		updatePostableImages ();
 
 		imagesToPost = new List<string> ();
 	}
@@ -68,37 +44,39 @@ public class ScrollViewManager : MonoBehaviour {
 		imagesToPost = imageBuffer;
 	}
 
+	public void selectAll() {
+		var images = GetComponentsInChildren<RawImage> ();
+
+		foreach(RawImage img in images) {
+			Toggle t = img.GetComponentInChildren<Toggle> ();
+			t.isOn = true;
+		}
+	}
 	public void updatePostableImages() {
 		foreach (Transform child in transform) {
 			Destroy (child.gameObject);
 		}
 
+
 		#if UNITY_EDITOR
 		//  Make sure pictures are loaded into resources
 		AssetDatabase.Refresh();
 		#endif
-		int counter = 0;
-		DirectoryInfo dir = new DirectoryInfo(Application.dataPath + "/Resources/");
-		//Debug.Log(dir);
-		FileInfo[] info = dir.GetFiles("*.png");
-		foreach (FileInfo f in info)
-		{
-			//Debug.Log(f);
-			string filename = f.Name;
-			//Debug.Log(filename);
 
-			if (!PlayerProfile.profile.postedPhotos.Contains (filename.Replace (".png", ""))) {
-				GameObject curPicture = (GameObject)Instantiate (newPicture);
-				Texture2D pic = new Texture2D (2, 2);
-				byte[] bytes = File.ReadAllBytes (Application.dataPath + "/Resources/" + filename);
-				pic.LoadImage (bytes);
-				RawImage r = (RawImage) curPicture.GetComponent<RawImage> ();
-				r.texture = pic;
-				curPicture.GetComponent<RawImage> ().name = filename.Replace (".png", "");
-				curPicture.transform.SetParent (this.transform, false);
-				curNames.Add (curPicture);
-			}
-			counter++;
+		DirectoryInfo dir = new DirectoryInfo(pathToUploadQueue);
+		FileInfo[] info = dir.GetFiles("*.png");
+		foreach (FileInfo photoFile in info) {
+			string filename = photoFile.Name;
+
+			GameObject curPicture = (GameObject)Instantiate (newPicture);
+			Texture2D pic = new Texture2D (2, 2);
+			byte[] bytes = File.ReadAllBytes (pathToUploadQueue + filename);
+			pic.LoadImage (bytes);
+			RawImage rawImage = (RawImage) curPicture.GetComponent<RawImage> ();
+			rawImage.texture = pic;
+			curPicture.GetComponent<RawImage> ().name = filename.Replace (".png", "");
+			curPicture.transform.SetParent (this.transform, false);
+			curNames.Add (curPicture);
 		}
 	}
 
