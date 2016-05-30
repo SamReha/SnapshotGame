6a54be10f309e3de5bca39e0304670115c332a51
Samuel Reha <sreha@adgpm.soe.ucsc.edu>
2016-05-30 15:04:15 -0700

    Camera is now centered well-enough on all resolutions

--- Snapshot/Assets/The Cam/SnapshotCam.cs
+++ Snapshot/Assets/The Cam/SnapshotCam.cs
@@ -53,8 +53,8 @@ namespace UnityStandardAssets.ImageEffects {
 			cameraAudio = GetComponent<AudioSource> ();
 
 			parent = GameObject.Find("PlayerCam");
-			cameraHeldUp = new Vector3(0.001f, 0.02f, -0.30f);
-			cameraHeldDown = new Vector3(0.293f, -0.499f, -0.055f);
+			cameraHeldUp   = new Vector3( 0.009f, 0.030f,-0.100f);
+            cameraHeldDown = new Vector3( 0.293f,-0.499f, 0.300f);
 
 			// Set portrait lens
 			/*curLens = GameObject.Find (currentLens);
