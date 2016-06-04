ac41809f1e3595d8505fece6ccea2d76e573a2ab
Sam Reha <samreha@Sams-MacBook-Pro.local>
2016-06-03 18:49:58 -0700

    Implemented delete save button

--- Snapshot/Assets/main_menu/scripts/MainMenuUI.cs
+++ Snapshot/Assets/main_menu/scripts/MainMenuUI.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 using System.Collections;
-
 
 public class MainMenuUI : MonoBehaviour {
 	public static AudioSource mainMenuSource;
 	public static bool prepMenu = false;
+	public GameObject confirmDeleteSave;
+	public GameObject deleteSaveSuccessful;
+
 	// Use this for initialization
 	void Start () {
+		confirmDeleteSave.SetActive (false);
+		deleteSaveSuccessful.SetActive (false);
+
 		if (!prepMenu) {
 			mainMenuSource = GetComponent<AudioSource> ();
 			AudioSource.DontDestroyOnLoad (mainMenuSource);
@@ -71,4 +77,42 @@ public class MainMenuUI : MonoBehaviour {
 		}
         SceneManager.LoadScene("credits");
     }
+
+	public void deleteSave() {
+		// First, wipe the data in the persistent datapath
+		DirectoryInfo persistentDataDirectory = new DirectoryInfo(Application.persistentDataPath);
+
+		foreach (FileInfo file in persistentDataDirectory.GetFiles()) {
+			file.Delete(); 
+		}
+		foreach (DirectoryInfo dir in persistentDataDirectory.GetDirectories()) {
+			dir.Delete(true); 
+		}
+
+		// Then, wipe the blog and memcard data from the resources directory.
+		DirectoryInfo postedImages = new DirectoryInfo(Application.dataPath + "/Resources/PostedImages/");
+		DirectoryInfo UploadQueue = new DirectoryInfo(Application.dataPath + "/Resources/UploadQueue/");
+
+		foreach (FileInfo file in postedImages.GetFiles()) {
+			file.Delete(); 
+		}
+		foreach (FileInfo file in UploadQueue.GetFiles()) {
+			file.Delete(); 
+		}
+
+		confirmDeleteSave.SetActive (false);
+		deleteSaveSuccessful.SetActive (true);
+	}
+
+	public void promptToDeleteSave() {
+		confirmDeleteSave.SetActive (true);
+	}
+
+	public void cancelDeleteSave() {
+		confirmDeleteSave.SetActive (false);
+	}
+
+	public void endSaveDialogue() {
+		deleteSaveSuccessful.SetActive (false);
+	}
 }
